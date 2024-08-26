ccda0b0540c864c1aabde94fe609d252f85f3249
Nick Elias <134450333+NickElias01@users.noreply.github.com>
2024-08-26 13:48:14 -0600

    ITERATION 4 - Update utils_nickelias.py

--- utils_nickelias.py
+++ utils_nickelias.py
@@ -1,4 +1,4 @@
-''' ITERATION 3
+''' ITERATION 4
 
 Module: Elias Analytics - Reusable Module for My Data Analytics Projects
 
@@ -6,13 +6,28 @@ This module provides a simple, reusable foundation for my analytics projects.
 
 Process:
 
-In this third iteration, I declare additional variables to show skills with different data types.
-
+In this fourth iteration, I introduce some basic statistics using Python.
+    - min() is a built in function to find the smallest value passed in
+    - max() is a built in function to find the largest value passed in
+    - The statistics module offers methods to calculate mean and standard deviation.
 '''
 
 #####################################
-# Declare global variables - keep byline at the end
-# We will use this information in a smarter byline
+# Import Modules at the Top
+#####################################
+
+# In Python, we can import modules to add extra tools and functions.
+# Below, we're importing:
+# - `statistics`: This gives us tools to calculate things like averages.
+# Use CTRL F and type statistics to see where it is used in the code. 
+# Did you find statistics.mean()?
+# Did you find statistics.stdev()?
+
+import statistics
+
+
+#####################################
+# Declare global variables
 #####################################
 
 # Boolean variable to indicate if the company has international clients
@@ -39,6 +54,23 @@ analytic_tools: list = ["Python","Github","mySQL","Tableau","Microsoft Power BI"
 # List of floats showing daily temperature highs in Denver, CO (degrees Fahrenheit)
 daily_temps: list = [95,93,88,86,90,90]
 
+#####################################
+# Calculate Basic Statistics 
+#   Do this BEFORE we declare the byline 
+#   So the values have been calculated 
+#   and are ready for use in the byline.
+#####################################
+
+# Calculate basic stats using built-in functions min(), max() and statistics module functions mean() and stdev().
+min_score: float = min(client_satisfaction_scores)  
+max_score: float = max(client_satisfaction_scores)  
+mean_score: float = statistics.mean(client_satisfaction_scores)  
+stdev_score: float = statistics.stdev(client_satisfaction_scores)
+
+min_temps: float = min(daily_temps)  
+max_temps: float = max(daily_temps)  
+mean_temps: float = statistics.mean(daily_temps)  
+stdev_temps: float = statistics.stdev(daily_temps)
 
 #####################################
 # Declare a global variable named byline. 
